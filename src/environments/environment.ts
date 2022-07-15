f22825b1ace0f23e025f20c6b42feb9697a9cce3
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-07-15 16:00:11 +0700

    update version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,13 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080',
-  firebaseConfig: {
-    apiKey: "AIzaSyD8-xixCVrK-1RrA6G-Ob5j88HoD72s0g0",
-    authDomain: "employee-app-f2600.firebaseapp.com",
-    projectId: "employee-app-f2600",
-    storageBucket: "employee-app-f2600.appspot.com",
-    messagingSenderId: "652664345747",
-    appId: "1:652664345747:web:40dd0895de56628b33e966",
-    measurementId: "G-853TR2HBGP"
+  firebase: {
+    apiKey: 'AIzaSyAJ1A_kLKyizWpBADruke7i_HW_bgj2VSk',
+    authDomain: 'finjob-management.firebaseapp.com',
+    projectId: 'finjob-management',
+    storageBucket: 'finjob-management.appspot.com',
+    messagingSenderId: '843687220997',
+    appId: '1:843687220997:web:26190aadbf47a340fbc96c'
   }
 };
 
